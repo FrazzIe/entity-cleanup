4557d920c05a55883d93d3ef6271f53203b04e75
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-08 15:27:34 +0000

    fix: onEntity -> onVehicle

--- cl_vehicle.js
+++ cl_vehicle.js
@@ -3,6 +3,6 @@
  * @param {string} poolName entity pool handle (CPed, CObject, CVehicle, CPickup)
  * @param {number} handle entity handle
  */
-function onEntity(poolName, handle) {
+function onVehicle(poolName, handle) {
 
 }

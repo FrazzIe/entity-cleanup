7c81bb067663afb62f9624c5464ad957d867549f
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-08 19:10:53 +0000

    feat: prevent null cleanup target

--- cl_cleanup.js
+++ cl_cleanup.js
@@ -4,7 +4,12 @@
  * @param {string[]} args array of command arguments
  */
 function onCleanup(src /* unused */, args) {
+	const target = args[0].toLowerCase();
 
+	if (target == null) {
+		console.log("Invalid argument passed");
+		return;
+	}
 }
 
 RegisterCommand("cleanup", onCleanup);

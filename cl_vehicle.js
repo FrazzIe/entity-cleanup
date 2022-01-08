4b80747ed5bc1e3a2fffd2ff8a544fb0f4c83c3f
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-08 20:00:22 +0000

    feat: rem onVehicle

--- cl_vehicle.js
+++ cl_vehicle.js
@@ -1,13 +1,4 @@
 /**
- * 
- * @param {string} poolName entity pool handle (CPed, CObject, CVehicle, CPickup)
- * @param {number} handle entity handle
- */
-function onVehicle(poolName, handle) {
-
-}
-
-/**
  * Remove all vehicles nearby client
  */
 function onVehicleCleanup() {

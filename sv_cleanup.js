5fbbb8462875ff712843d52caa7a36032ed770e8
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-08 21:18:03 +0000

    feat: onCleanupFinish func

---
+++ sv_cleanup.js
@@ -0,0 +1,9 @@
+/**
+ * Remove collection of entities from world
+ * @param {number[]} entities collection of entities
+ */
+function onCleanupFinish(entities) {
+	
+}
+
+on("entity-cleanup:finish", onCleanupFinish);

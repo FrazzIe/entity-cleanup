49b17bcb4ca50906f8a1726d564430ccd71702d0
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-08 15:37:01 +0000

    feat: onCleanup func, cleanup cmd

---
+++ cl_cleanup.js
@@ -0,0 +1,10 @@
+/**
+ * Command handler for entity cleanup
+ * @param {null} src 
+ * @param {string[]} args array of command arguments
+ */
+function onCleanup(src /* unused */, args) {
+
+}
+
+RegisterCommand("cleanup", onCleanup);

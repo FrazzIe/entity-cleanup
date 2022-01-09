1b36e32f1012f4a26c51f691fe9fe20fc4b87fc3
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-09 12:47:58 +0000

    feat: collect peds for deletion

--- cl_ped.js
+++ cl_ped.js
@@ -2,7 +2,23 @@
  * Collect peds nearby client for deletion
  */
 function onCleanupPed() {
+	const peds = [];
 
+	forEachInGamePool("CPed", (handle) => {
+		// ignore if ped isn't networked
+		if (NetworkGetEntityIsNetworked(handle) == false) {
+			return;
+		}
+
+		// store ped network id for deletion
+		peds[peds.length] = NetworkGetNetworkIdFromEntity(handle);
+	});
+
+	// only send if there are peds to delete
+	if (peds.length > 0) {
+		// send collection of peds for deletion
+		emitNet("entity-cleanup:finish", peds);
+	}
 }
 
 on("entity-cleanup:ped", onCleanupPed);

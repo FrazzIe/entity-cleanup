42a75e96745f38fc94761ed523566ad054d79dd9
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-01-09 00:12:50 +0000

    feat: delete entities

--- sv_cleanup.js
+++ sv_cleanup.js
@@ -3,7 +3,10 @@
  * @param {number[]} entities collection of entities
  */
 function onCleanupFinish(entities) {
-	
+	// delete entities
+	for (let i = 0; i < entities.length; i++) {
+		DeleteEntity(entities[i]);
+	}
 }
 
 on("entity-cleanup:finish", onCleanupFinish);
